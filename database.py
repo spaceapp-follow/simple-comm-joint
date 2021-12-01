a024692e78042844e515ca15436ff859a56b517e
Yılmaz <emirhanyilmazguney@hotmail.com>
2021-12-01 15:02:59 +0300

    Database class constructor has been updated

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
 #Yılmaz Ebrar
-
+import mysql.connector
+import pandas as pd
 class Database():
-    def __init__(self):
-        pass
+    def _init_(self,host,user,password,csvPath):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.mydb = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password
+            )
+        self.cursor = self.mydb.cursor()
+        csv = pd.read_csv(csvPath)
+        id = csv[['id']].values.tolist()
+        coordinates = csv[['coordinates']].values.tolist()
+        time = csv[['time']].values.tolist()
+        self.data = [id,coordinates,time]
     def insertData():
         pass
     

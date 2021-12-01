1c2d4edcb7faeed9efdc0fd3a722685a13f23e7f
Ebrar Çakmak <ebrar.cakmak@metu.edu.tr>
2021-12-01 14:58:17 +0300

    insert data function has been updated

--- database.py
+++ database.py
@@ -4,6 +4,10 @@ class Database():
     def __init__(self):
         pass
     def insertData():
-        pass
+        self.cursor.execute("USE {}".format(database))
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS {} (id INT, coordinates INT, time INT)".format(table))
+        sqlquery = "INSERT INTO {} (id, coordinates, time) VALUES (%s, %s, %s)".format(table)
+        self.cursor.executemany(sqlquery, self.data)
+        self.mydb.commit()
     
     

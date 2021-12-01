cf2162ec53959148d5263b1599e1bad446525374
Ahmet Akman <ahmet.akman@metu.edu.tr>
2021-12-01 23:12:09 +0300

    pubsubclasses

--- communication.py
+++ communication.py
@@ -1,13 +1,48 @@
 #AHMET ve ENES
+import zmq
 
 class Communication():
     def __init__(self):
+        self.address = "tcp://*:5555"
+
+
         pass
-    def setVariablesPubSub():
+
+    
+    def setVariablesPubSub(self):
+
         pass
-    def transmitPubSub():
-        pass
-    def receivePubSub():
+    def transmitPubSub(self,packed_data,length):
+
+            pubctx = zmq.Context()
+            self.sender = pubctx.socket(zmq.PUB)
+            self.sender.bind(self.address)
+
+            for self.packet_count in range(length):
+                self.message = packed_data[self.packet_count]   
+                self.sender.send_pyobj(self.message)
+
+
+        
+    def receivePubSub(self,length):
+        subctx = zmq.Context()
+
+        receiver = subctx.socket(zmq.SUB)
+
+        receiver.connect(self.address)
+        receiver.setsockopt_string(zmq.SUBSCRIBE, '')
+        self.all_data = []
+        while True:
+
+            message = receiver.recv_pyobj()
+    
+            print(message)
+
+
+            if len(self.all_data) < length :
+                self.all_data.append(message)
+            else:
+                break
         pass
     def setVariablesPushPull():
         pass

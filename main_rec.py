df8cbc5255bd83c70fd95ee1d55c55c7e680f9a4
Ebrar Çakmak <ebrar.cakmak@metu.edu.tr>
2021-12-07 13:55:33 +0300

    mains

--- main_rec.py
+++ main_rec.py
@@ -1,23 +1,36 @@
 from communication import Communication
 from data_generator import DataGenerator
-#from database import Database
+from database import Database
 
 communicator=Communication()
 dataGen = DataGenerator()
 
-"""
-host=""
-user=""
-password=""
-csvPath=""
-base=Database(host,user,password,csvPath)
-database=""
-table=""
-base.insertData(database,table)
-"""
+
+
+
 received_data = communicator.receivePushPull()
 print(received_data)
 dataGen.packed_data=received_data
 dataGen.unpack()
+
 print(dataGen.raw_data)
+
+host="127.0.0.1"
+user="ebrar"
+password="Qdvnl.22"
+csvPath=""
+
+base=Database()
+base.host = host
+base.user = user
+base.password = password
+base.csvPath = csvPath
+base.data = dataGen.raw_data
+database="tubitakdatabase"
+table="data1"
+
+base.setVar()
+base.insertData(database,table)
+
+
 
